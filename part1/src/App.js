5a569749e025b94335b8969f225124fb2418fbc4
michael <michaelmhonda@gmail.com>
2022-01-08 11:50:09 -0500

    multiple components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,37 @@
 import React from 'react'
 
-const Hello = () => {
-  
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app re-created by <a href="https://github.com/mikemike112-1">mikemike112-1</a>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
+
   return (
-    <div>
+    //<div> //being replaced with an empty element
+    //helps the DOM not generate extra div elements 
+    <>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age = {age}/>
+      <Footer />
+    </>
+    //</div>
   )
 }
 
