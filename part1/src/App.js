a2d568687d38f7c2299fec998ef7ca1e67d6c50e
michael <michaelmhonda@gmail.com>
2022-01-08 11:30:38 -0500

    first component

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
-const App = () => (
-  <div>
-    <p>Hello World</p>
-  </div>
-)
+
+const App = () => {
+  const now = new Date()
+  const a = 10
+  const b = 20
+  
+    return (
+    <div>
+      <p>Hello World, it is {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+    </div>
+  )
+}
 
 export default App 
